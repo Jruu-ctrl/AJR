1563ad0de252f20967e5c3c005f60eed0673a26d
Jruu <jruanticheat@gmail.com>
2025-03-29 18:25:24 +0100

    create warns file and delete useless commands and cmd file

---
+++ cmds/warns.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+from discord import app_commands
+import discord
+
+
+class warn_commands(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
+    @commands.bot_has_permissions(kick_members=True)
+    @app_commands.command(name="warn", description="Warn a user for rule violations. The warning is logged for moderation purposes.")
+    @app_commands.describe(
+        user="The user to warn",
+        reason="Reason for the warning"
+        )
+    async def warn(self,interaction:discord.Interaction,user:discord.Member,reason:str =""):
+        admin=interaction.user.name
+        if reason != "":
+            await interaction.response.send_message(f"Admin: `{admin}`\nWarned user: `{user}` -> ID: ||{user.id}||\nReason: {reason}")
+            await user.send(f"You have been warned by: `{admin}`\nReason: `{reason}`")
+        else:
+            await interaction.response.send_message(f"Admin: `{admin}`\nWarned user: `{user}` -> ID: ||{user.id}||")
+            await user.send(f"u have been warned from: `{admin}`")
+
+
+async def setup(bot):
+    await bot.add_cog(warn_commands(bot))
+
+
+
--- main.py
+++ main.py
@@ -18,21 +18,32 @@ def run():
         print(bot.user)
         print(bot.user.id)
 
+
         for file in settings.CMDS_DIR.glob("*.py"):
             await bot.load_extension(f"cmds.{file.name[:-3]}")
-        
+
         bot.tree.copy_global_to(guild=settings.GUILDS_ID)
         await bot.tree.sync(guild=settings.GUILDS_ID)
 
-    @bot.tree.command()
-    async def reload(interaction:discord.Interaction,file:str):
-        await bot.reload_extension(f"cmds.{file.lower()}")
-        await interaction.response.send_message(f"✅Successfully reloaded {file}")
+    @bot.tree.context_menu(name="Join")
+    async def join(interaction:discord.Interaction, member:discord.Member):
+        await interaction.response.send_message(f"`{member.name}` joined on `{member.joined_at.strftime("%Y/%m/%d %H-%M-%S")}`", ephemeral=True)
+
+
+    @bot.tree.command(description="Module refresh initiated")
+    @app_commands.describe(module="Module to refresh:")
+    async def reload(interaction:discord.Interaction,module:str):
+        if interaction.guild.get_role(1290411720791294093) in interaction.user.roles :
+            await bot.reload_extension(f"cmds.{module.lower()}")
+            await interaction.response.send_message(f"✅Successfully reloaded {module}",ephemeral=True)
+        else:
+            await interaction.response.send_message(f"You do not have the `{interaction.guild.get_role(1290411720791294093).name}` role required to execute this command.",ephemeral=True)
+
 
     @bot.tree.error
     async def on_app_command_error(interaction:discord.Interaction,error):
         if isinstance(error,app_commands.CommandInvokeError):
-            await interaction.response.send_message("eurror globaly",ephemeral=True)
+            await interaction.response.send_message("The command you entered is incorrect. Please check the syntax and try again.",ephemeral=True)
         
 
 
